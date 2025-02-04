9818bc47ceddd40d36803034740b31f99f80e466
Timothe Lebrun <timothe463@icloud.com>
2025-02-04 22:26:58 +0100

    feat-list: reajust html structure

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -1,28 +1,34 @@
 import DataTable from "datatables.net-react";
-import DT from "datatables.net-dt";
+import DT from "datatables.net";
 import {useSelector} from "react-redux";
 function List() {
+    const root = document.getElementById("root");
+    root.classList.add('list');
     DataTable.use(DT);
     const rows = useSelector(state=> state.table.rows);
     return (
-        <div id="employee-div" className="container">
-            <h1>Current Employees</h1>
-            <DataTable data={rows} >
-                <thead>
-                <tr>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Start Date</th>
-                    <th>Department</th>
-                    <th>Date of birth</th>
-                    <th>Street</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Zipcode</th>
-                </tr>
-                </thead>
-            </DataTable>
-            <a href="/">Home</a>
+        <div className="content">
+                <header className="header">
+                    <h1>HRnet</h1>
+                    <a className="link" href="/">Home &gt;</a>
+                </header>
+            <div id="employee-div" className="container">
+                <DataTable data={rows} className="dataTable">
+                    <thead>
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Start Date</th>
+                        <th>Department</th>
+                        <th>Date of birth</th>
+                        <th>Street</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zipcode</th>
+                    </tr>
+                    </thead>
+                </DataTable>
+            </div>
         </div>
     );
 }
